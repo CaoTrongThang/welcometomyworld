f710d41be67d0d7dde7791336fb42b6b8adafc79
CaoTrongThang <fptisthebest@gmail.com>
2026-04-09 05:34:46 +0700

    start adding armors

--- src/main/java/com/trongthang/welcometomyworld/features/DimensionRestrictionsHandler.java
+++ src/main/java/com/trongthang/welcometomyworld/features/DimensionRestrictionsHandler.java
@@ -4,11 +4,18 @@ import com.trongthang.welcometomyworld.world.dimension.VoidDimension;
 import com.trongthang.welcometomyworld.world.dimension.WhiteDimension;
 import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
 import net.fabricmc.fabric.api.event.player.UseBlockCallback;
+import net.minecraft.block.Blocks;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.particle.ParticleTypes;
 import net.minecraft.registry.Registries;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.BlockPos;
 
 public class DimensionRestrictionsHandler {
     public static void registerEvents() {
@@ -41,7 +48,29 @@ public class DimensionRestrictionsHandler {
                     }
 
                     if (isWaterBucket) {
-                        return ActionResult.FAIL;
+                        if (!world.isClient) {
+                            BlockPos targetPos = hitResult.getBlockPos();
+                            if (!world.getBlockState(targetPos).isReplaceable()) {
+                                targetPos = targetPos.offset(hitResult.getSide());
+                            }
+
+                            // Smoke and sound like the Nether
+                            world.playSound(null, targetPos, SoundEvents.BLOCK_FIRE_EXTINGUISH, SoundCategory.BLOCKS,
+                                    0.5f, 2.6f + (world.random.nextFloat() - world.random.nextFloat()) * 0.8f);
+                            ((ServerWorld) world).spawnParticles(ParticleTypes.LARGE_SMOKE, targetPos.getX() + 0.5,
+                                    targetPos.getY() + 0.5, targetPos.getZ() + 0.5, 8, 0.2, 0.2, 0.2, 0.0);
+
+                            // 50/50 chance to turn into packed ice or evaporate
+                            if (world.random.nextFloat() < 0.5f) {
+                                if (world.getBlockState(targetPos).isReplaceable()) {
+                                    world.setBlockState(targetPos, Blocks.PACKED_ICE.getDefaultState());
+                                }
+                            }
+
+                            // Return empty bucket
+                            player.setStackInHand(hand, new ItemStack(Items.BUCKET));
+                        }
+                        return ActionResult.SUCCESS;
                     }
                 }
             }
